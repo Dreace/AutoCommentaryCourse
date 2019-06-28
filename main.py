b64c98f9b39523fb041dbb5c9fba589ed41ebcd8
Dreace233 <Dreace@Foxmail.com>
2019-06-29 00:15:35 +0800

    处理验证码

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# coding=utf-8
+import json
+import os
+import random
+import re
+import traceback
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import bs4
 import requests
-import traceback
-import random
-import json
 import yaml
-import os
-import re
 
 Max = 0
 Min = 0
@@ -15,15 +18,33 @@ Summarize = ""
 def login(session, username, password):
     try:
         login_url = "http://zxpj.nuc.edu.cn"
-        post_data = {
-            "username": username,
-            "password": password
-        }
-        html = session.post(login_url, data=post_data, timeout=5).content.decode("utf-8")
+        vcode_url = "http://zxpj.nuc.edu.cn/kaptcha"
+        flag = True
+        while flag:
+            vcode = session.get(vcode_url).content
+            with open("vcode.jpg","wb") as vcode_file:
+                vcode_file.write(vcode)
+            vcode_image = mpimg.imread("vcode.jpg")
+            plt.imshow(vcode_image)
+            plt.axis('off')
+            plt.show()
+            vcode_text = raw_input("验证码：")
+            post_data = {
+                "username": username,
+                "password": password,
+                "kaptcha": vcode_text
+            }
+            html = session.post(login_url, data=post_data, timeout=5).content.decode("utf-8")
+            if html.find(u"验证码错误") != -1:
+                print ("验证码错误")
+            else:
+                flag = False
         if html.find(u"评课任务") != -1:
             return session, html
+        elif html.find(u"账号密码错误") != -1:
+            return False, "账号密码错误"
         else:
-            return False, ""
+            return False, "登陆失败"
     except:
         print(traceback.format_exc())
         return False, ""
@@ -45,8 +66,6 @@ def start():
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/73.0.3683.86Safari/537.36"}
     base_url = "http://zxpj.nuc.edu.cn/login"
-    username = ""
-    password = ""
     proxies = {}
     if os.path.exists("config.yaml"):
         yaml_file = open("config.yaml", encoding='UTF-8')
@@ -59,18 +78,24 @@ def start():
         Min = y["Min"]
         Summarize = y["Summarize"]
     else:
-        s = input("是否已连接校园网（Y/N）：")
+        s = raw_input("是否已连接校园网（Y/N）：")
         if s != "y" and s != "Y":
+            print ("需要校园网环境")
+            return
             proxies = {"http": "http://47.101.211.35:888", "https": "http://47.101.211.35:888"}
-        username = input("账号：")
-        password = input("密码：")
-        Max = input("最大随机分数（默认 99）：")
+        username = raw_input("账号：")
+        password = raw_input("密码：")
+        Max = raw_input("最大随机分数（默认 99）：")
         if Max == "":
             Max = 99
-        Min = input("最小随机分数（默认 80）：")
+        else:
+            Max = int(Max)
+        Min = raw_input("最小随机分数（默认 80）：")
         if Min == "":
             Min = 80
-        Summarize = input("总结（默认 “无”）：")
+        else:
+            Min = int(Min)
+        Summarize = raw_input("总结（默认 “无”）：")
         if Summarize == "":
             Summarize = u"无"
     session = requests.session()
@@ -78,10 +103,10 @@ def start():
     r = session.get(base_url)
     headers["Cookie"] = "shiroCookie=" + session.cookies["shiroCookie"]
     session.headers = headers
-    session, html = login(session, username, password)
+    session, res = login(session, username, password)
     if session:
         print("登陆成功")
-        soup = bs4.BeautifulSoup(html, "html.parser")
+        soup = bs4.BeautifulSoup(res, "html.parser")
         div = soup.find_all("div", class_="am-active")[0]
         xxqs = div.find_all(class_="xxq")
         if len(xxqs) < 1:
@@ -95,7 +120,7 @@ def start():
             print("完成\n")
             i += 1
     else:
-        print("登陆失败")
+        print(res)
 
 
 def commentary_course(session, id):
@@ -115,9 +140,9 @@ def commentary_course(session, id):
     session.headers = headers
     r = session.get(base_url, ).content.decode("utf-8")
     res = re.search(u"<strong>授课人：</strong>(.*?)</div>", r)
-    print("授课人：%s" % res.group(1))
+    print(u"授课人：%s" % res.group(1))
     res = re.search(u"<strong>所授课程：</strong>(.*?)</div>", r)
-    print("所授课程：%s" % res.group(1))
+    print(u"所授课程：%s" % res.group(1))
     soup = bs4.BeautifulSoup(r, "html5lib")
     eid = soup.find_all(id="eid")[0]["value"]
     trs = soup.find_all("tr")[1:-1]
@@ -127,7 +152,7 @@ def commentary_course(session, id):
         a = tr.find(rowspan="1").string
         item = tr.find(class_="am-form-contentid")["value"]
         score = random.randint(Min, Max)
-        print("%s：%s" % (a, score))
+        print(u"%s：%s" % (a, score))
         content.append({int(item): score})
     info = {}
     info["content"] = content
@@ -152,4 +177,4 @@ def commentary_course(session, id):
 
 if __name__ == "__main__":
     start()
-    input("按任意键退出")
+    raw_input(u"按任意键退出")
